0b636c5e8c630735b0c82ab2bcf1c13f3d3efe62
gonzaloantolin <g.antolin.2025@alumnos.urjc.es>
2026-02-10 16:45:01 +0100

    feb 10, changes (Session03)

---
+++ Session03/dna_count.py
@@ -0,0 +1,39 @@
+def seq_count(seq):
+    a = 0
+    c = 0
+    g = 0
+    t = 0
+    for i in seq:
+        if i == "A":
+            a += 1
+        elif i == "C":
+            c += 1
+        elif i == "G":
+            g += 1
+        elif i == "T":
+            t += 1
+    return a, c, g, t
+
+seq = input("Introduce the sequence: ").upper()
+dna_len = len(seq)
+
+a, c, g, t = seq_count(seq)
+
+print("Total length:", dna_len)
+print("A:", a)
+print("C:", c)
+print("G:", g)
+print("T:", t)
+
+#other way
+print("____________other way (WITH FILES)_____________")
+#other way
+
+seq = input("Introduce the sequence: ").upper()
+print("Total length:", len(seq))
+bases = {"A": 0, "C": 0, "T": 0, "G": 0}
+for base in seq:
+    if base in bases:
+        bases[base] += 1
+for base, count in bases.items():
+    print(f'{base}: {count}')
---
+++ Session03/dna_count_file.py
@@ -0,0 +1,69 @@
+def total_bases(dna_list):
+    a = c = g = t = 0
+    for seq in dna_list:
+        for base in seq:
+            if base == "A":
+                a += 1
+            elif base == "C":
+                c += 1
+            elif base == "G":
+                g += 1
+            elif base == "T":
+                t += 1
+    return a + c + g + t
+
+def different_bases(dna_list):
+    a = c = g = t = 0
+    for seq in dna_list:
+        for base in seq:
+            if base == "A":
+                a += 1
+            elif base == "C":
+                c += 1
+            elif base == "G":
+                g += 1
+            elif base == "T":
+                t += 1
+    return a, c, g, t
+
+dna_list = ["AGTACACTGGT", "ACCAGTGTACT", "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"]
+a, c, g, t = different_bases(dna_list)
+print("The total number of bases is:", total_bases(dna_list))
+print("A:", a)
+print("C:", c)
+print("G:", g)
+print("T:", t)
+
+#other way
+print("____________other way (WITH FILES)_____________")
+#other way
+
+#lines = ["AGTACACTGGT", "ACCAGTGTACT", "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"]
+#print("from variable:", lines)
+
+f = open("dna.txt.", "r") #one way of opening files
+#code goes here
+lines = f.readlines()
+f.close()
+
+with open("dna.txt.", "r") as f: #another way of opening files (this ones better)
+    lines = f.readlines()
+
+total_number = 0
+bases = {"A": 0, "C": 0, "T": 0, "G": 0}
+
+for seq in lines:
+    seq = seq.strip() #remove spaces and newline characters at the end of the string
+    total_number += len(seq)
+
+    for base in seq:
+        if base in bases:
+            bases[base] += 1
+
+print("Total number of bases:", total_number)
+
+for base, count in bases.items():
+    print(f'{base}: {count}')
+
+
+#from dna_count import count_bases
