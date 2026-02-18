496c8ec5c4b796f4d42776341f926128865074d0
gonzaloantolin <g.antolin.2025@alumnos.urjc.es>
2026-02-18 16:45:12 +0100

    feb 18, changes (Session04)

--- Session04/S04/find_exons.py
+++ Session04/S04/find_exons.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 file_path = Path("sequences/ADA.txt")
 gene_content = file_path.read_text().split("\n")
+
 gene_lines = gene_content[1:]
 gene_sequence = ""
+
 for line in gene_lines:
     gene_sequence += line
 
@@ -22,4 +24,23 @@ for line in exon_content:
 if current_exon != "":
     exons.append(current_exon)
 
-print(current_exon)
+chromosome_max = 44652852
+
+for i in range(len(exons)):
+
+    exon = exons[i]
+    i = gene_sequence.find(exon)
+
+    length = len(exon)
+
+    start = chromosome_max - i
+    end = chromosome_max - (i + length - 1)
+
+    print("Exon", i+1)
+    print("Length:", length)
+    print("Start:", start)
+    print("End:", end)
+    print("----------------------")
+
+
+
